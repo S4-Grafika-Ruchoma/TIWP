3270d530caab6a94261e6c6f26540470060f5ae9
Alan <alanbiegun13@gmail.com>
2019-12-14 18:53:15 +0100

    init

---
+++ Controllers/RazorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TIWP_APS_NET_FRAMEWORK_CORE.Controllers
+{
+    public class RazorController : Controller
+	{
+		public IActionResult Intro()
+		{
+			return View();
+		}
+		public IActionResult Intro2()
+		{
+			return View();
+		}
+		public IActionResult Intro3()
+		{
+			return View();
+		}
+	}
+}
