e2a29924905b53ac2d69cadc811850d4d05169ed
Alan <alanbiegun13@gmail.com>
2019-12-14 20:20:35 +0100

    qweqweqwe

--- Controllers/RazorController.cs
+++ Controllers/RazorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TIWP_APS_NET_FRAMEWORK_CORE.Models;
 
 namespace TIWP_APS_NET_FRAMEWORK_CORE.Controllers
 {
@@ -19,6 +20,39 @@ namespace TIWP_APS_NET_FRAMEWORK_CORE.Controllers
 		public IActionResult Intro3()
 		{
 			return View();
+		}
+
+		public IActionResult Dane()
+		{
+			return View();
+		}
+		public IActionResult Dane2()
+		{
+			return View();
+		}
+
+		public IActionResult Modele()
+		{
+			var model = new Auto()
+			{
+				Marka = "Volvo",
+				Model = "V40"
+			};
+			
+			//var model = new List<Auto>(){
+			//	new Auto()
+			//	{
+			//		Marka = "Volvo",
+			//		Model = "V40"
+			//	},
+			//	new Auto()
+			//	{
+			//		Marka = "Volvo 2 ",
+			//		Model = "V4022"
+			//	}
+			//};
+
+			return View(model);
 		}
 	}
 }
---
+++ Models/Auto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TIWP_APS_NET_FRAMEWORK_CORE.Models
+{
+	public class Auto
+	{
+		public string Marka { get; set; }
+		public string Model { get; set; }
+
+	}
+}
